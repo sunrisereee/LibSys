5f9d7dd7dc5690e34d00ce1d7b8afd839757d84a
Ivan Mur <Kanifolochka@yandex.ru>
2024-10-15 23:15:52 +0300

    Adding Models

--- LybrarySystem/admin.py
+++ LybrarySystem/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Book
+from .models import Reader
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    model = Book
+    list_display = ['id', 'bookAuthor', 'bookName', 'bookDate', 'bookPageCount', 'bookInstances']
+
+admin.site.register(Book, BookAdmin)
+class ReaderAdmin(admin.ModelAdmin):
+    model = Reader
+    list_display = ['id', 'readerFirstName', 'readerSecondName', 'bookBirthday', 'hashPassword']
+
+admin.site.register(Reader, ReaderAdmin)
--- LybrarySystem/models.py
+++ LybrarySystem/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    bookAuthor = models.TextField()
+    bookName = models.TextField() # models.TextField()
+    bookDate = models.DateField()
+    bookPageCount = models.IntegerField()
+    bookInstances = models.IntegerField()
+    #photolikecount = models.IntegerField()
+    def __str__(self):
+        return self.bookName
+
+    def __repr__(self):
+        return self.bookName
+
+class Reader(models.Model):
+    readerFirstName = models.TextField()
+    readerSecondName = models.TextField() # models.TextField()
+    bookBirthday = models.DateField()
+    hashPassword = models.TextField()
+    #bookInstances = models.IntegerField()
+    #photolikecount = models.IntegerField()
+    def __str__(self):
+        return self.readerFirstName+' '+self.readerSecondName
+
+    def __repr__(self):
+        return self.readerFirstName+' '+self.readerSecondName
+
+class BookANDReader(models.Model):
+    bookID = models.TextField()
+    readerID = models.TextField() # models.TextField()
+    dateIssue  = models.DateField()
+    dateIreceipt = models.TextField()
+    #bookInstances = models.IntegerField()
+    #photolikecount = models.IntegerField()
+    def __str__(self):
+        return self.bookID+' '+self.readerID
+
+    def __repr__(self):
+        return self.bookID+' '+self.readerID
