b86d16b63d3e3db1b72c2ec8033b479374deff63
sunrisereee <gkarpeykin@mail.ru>
2024-10-15 21:03:46 +0300

    HTML code, urls.py

--- LybrarySystem/views.py
+++ LybrarySystem/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def books_list(request):
+    return render(request, 'books.html')
+
+def readers(request):
+    return render(request, 'readers.html')
+
+def library(request):
+    return render(request, 'library.html')
