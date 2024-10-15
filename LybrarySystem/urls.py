145d5e1fd9502762113227547b8cd5d2c121caba
Ivan Mur <Kanifolochka@yandex.ru>
2024-10-16 00:34:44 +0300

    Fixing HTML and add new views

--- LybrarySystem/urls.py
+++ LybrarySystem/urls.py
@@ -8,7 +8,13 @@ urlpatterns = [
    path('books', views.books_list, name="books"),
    path('readers', views.readers, name="readers"),
    path('library', views.library, name="library"),
-   #path('login', views.login, name="login"),
+   path('addBook', views.addBook),
+   path('editBook', views.editBook),
+   path('deleteBook', views.deleteBook),
+
+   path('addReader', views.addReader),
+   path('editReader', views.editReader),
+   path('deleteReader', views.deleteReader),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
