34b743844f6a9b3fec63368391c294810c44840f
sunrisereee <gkarpeykin@mail.ru>
2024-10-16 00:38:38 +0300

    Add output lists views

--- LybrarySystem/views.py
+++ LybrarySystem/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 
+from .models import Book, Reader
 def index(request):
     return render(request, 'index.html')
 
@@ -12,3 +13,19 @@ def readers(request):
 
 def library(request):
     return render(request, 'library.html')
+
+def bookList(request):
+    books = Book.objects.all()
+    context = {
+        'render_task': books,
+        'page_title':'Список книг',
+    }
+    return render(request, 'books.html', context)
+
+def readersList(request):
+    readers = Reader.objects.all()
+    context = {
+        'render_task': readers,
+        'page_title':'Список читателей',
+    }
+    return render(request, 'library.html', context)
